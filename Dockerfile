c3059782bc8370c894adcbf69de28fde7622a9a0
collins <chigbo.c.o@gmail.com>
2023-12-02 18:45:19 +0100

    edited dockerfile and vite.config

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,27 @@
-FROM node:14 
+FROM node:21-alpine AS BUILD_IMAGE
 
 WORKDIR /app
 
-COPY package.json .
+COPY package*.json ./
+
 
 RUN npm install
 
 COPY . .
+
 RUN npm run build
 
+
+FROM node:21-alpine AS PRODUCTION_IMAGE
+
+WORKDIR /app
+
+COPY --from=BUILD_IMAGE /app/dist/ /app/dist/
 EXPOSE 5173 
 
-CMD ["npm", "run", "dev"]
+COPY package*.json ./
+COPY vite.config.ts .
+
+RUN npm install typescript 
+
+CMD ["npm", "run", "preview"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- server: {
-    proxy: {
+  preview: {
+      host: true,
+      port:5173,
+      proxy: {
         '/api': {
             target: 'https://challenge.digitalepatientenhilfe.de',
             changeOrigin: true,
-            //rewrite: (path) => path.replace(/^\/api/, ''),
         }
     }
 }
